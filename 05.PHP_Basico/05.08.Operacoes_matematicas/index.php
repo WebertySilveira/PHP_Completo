d104837608cb0dc1ebdc303e1f38d2fd846e5872
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 22:55:18 -0300

    Operações Matemáticas

---
+++ 05.PHP_Basico/05.08.Operacoes_matematicas/index.php
@@ -0,0 +1,18 @@
+<?php
+/*SOMA*/
+    echo 5 + 5, '<br>';
+/*SUBTRAÇÃO*/
+    echo 5 - 5, '<br>';
+/*DIVISAO*/
+    echo 5 / 5, '<br>';
+/*SOMA E SUBTRAÇÃO*/
+    echo 5 + 5 - 5, '<br>';
+/*SOMA E DIVISAO*/
+    echo 5 + 5 / 5, '<br>';
+/*SOMA E MULTIPLICAÇÃO*/ 
+    echo 5 + 5 * 5, '<br>';
+/*SOMA E RESTO DA DIVISÃO*/
+    echo 5 + 5 % 5, '<br>';
+/*VALORES MUDAM IGUAL NA MATEMÁTICA*/
+    echo (5 + 5) % 5, '<br>';
+?>
