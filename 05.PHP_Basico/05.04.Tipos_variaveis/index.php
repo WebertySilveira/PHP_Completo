b6cbf0c977fc2ebc815a3945223f9023f6e51c35
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 22:07:45 -0300

    Tipos de Variáveis

---
+++ 05.PHP_Basico/05.04.Tipos_variaveis/index.php
@@ -0,0 +1,21 @@
+<?php
+    /*String*/
+    $nome = 'Weberty';
+
+    /*Inteiro*/
+    $idade = 22;
+
+    /*Boolean*/
+    $bool = true;
+
+    /*Double*/
+    $double = 10.99;
+
+    /*Printando variáveis em tela*/
+    echo 'String: ' .$nome. '<br>';
+    echo 'Inteiro: ' .$idade. '<br>';
+    echo 'True: ' .$bool. '<br>';
+    echo 'Double: ' .$double. '<br>';
+
+
+?>
