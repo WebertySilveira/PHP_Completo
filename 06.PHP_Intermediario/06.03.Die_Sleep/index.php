71968f2d6d14b3e95a65f42d668c15a4a14991f3
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-25 15:12:53 -0300

    Funçoes Die() e Sleep()

---
+++ 06.PHP_Intermediario/06.03.Die_Sleep/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    sleep(3);
+    echo "Atrasou 3 segundos <br>";
+    
+
+    
+    $nome = 'Bernardo';
+
+    if($nome == 'João'){
+        echo 'Tudo certo';
+    }else {
+        die("Script parou a execução");
+    }
+?>
