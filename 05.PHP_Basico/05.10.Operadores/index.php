46e7325269567903a3b9485f86193f03466d0e5e
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 23:11:28 -0300

    Operadores

---
+++ 05.PHP_Basico/05.10.Operadores/index.php
@@ -0,0 +1,30 @@
+<?php
+    $nome1 = 'Weberty';
+    $nome2 = 'Silveira';
+
+
+    if($nome1 == $nome2){
+        echo 'verdade'; 
+    }
+    
+    if($nome1 != $nome2){
+        echo 'Diferente'; 
+    }
+
+
+    echo '<br><br><br>';
+
+
+    $n1 = 10;
+    $n2 = '10';
+    
+    if($n1 == $n2){
+        echo 'Igual'; 
+    }
+    
+    if($n1 === $n2){
+        echo 'Igual e mesmo tipo'; 
+    }
+
+
+?>
