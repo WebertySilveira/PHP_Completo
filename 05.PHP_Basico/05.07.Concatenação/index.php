84315465f335838b3555ae79ab861b9206c5e818
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 22:48:20 -0300

    Concatenação

---
+++ 05.PHP_Basico/05.07.Concatenação/index.php
@@ -0,0 +1,11 @@
+<?php
+/*Mesmo resultado*/
+    $valor = 13;
+    define('VALOR', '13');
+
+    echo 'O valor total é ' .$valor. ' reais <br>';
+    echo 'O valor total é ' .VALOR. ' reais <br>';
+
+/*Exemplo com html*/
+    echo "<div class=\"Classe\"> Conteúdo</div>";
+?>
