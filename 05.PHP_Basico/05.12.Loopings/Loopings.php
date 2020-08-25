73742a5a8f758fecbef409454bf809e71ba9a1b1
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-25 00:02:02 -0300

    Estruturas de Repetição

---
+++ 05.PHP_Basico/05.12.Loopings/Loopings.php
@@ -0,0 +1,19 @@
+<?php
+	for ($contador = 0; $contador; $contador++){
+		echo $contador;
+	}
+
+	
+	$cont = 0;
+	while($cont < 10){
+		echo $cont;
+		$cont +=1;
+	}
+
+
+	$con = 11;
+	do{
+		echo "teste";
+	}while($contador == 10);
+
+?>
