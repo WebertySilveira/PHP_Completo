a313cc5058d666748371676c3fbcf632c96e9b9c
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 23:05:02 -0300

    Condições

---
+++ 05.PHP_Basico/05.09.Condicoes/index.php
@@ -0,0 +1,10 @@
+<?php
+    $n1 = 10 + 5;
+    $n2 = 9 + 6;
+
+    if($n1 == $n2){
+        echo '<div style="width:300px; height:300px;background:red;">';
+    } else{
+        echo 'Condição inválida';
+    }
+?>
