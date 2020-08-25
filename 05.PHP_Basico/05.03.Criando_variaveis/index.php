34618450183bb93d19a4e2b975952b036c484313
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-08-24 21:56:37 -0300

    Criando Variáveis

---
+++ 05.PHP_Basico/05.03.Criando_variaveis/index.php
@@ -0,0 +1,20 @@
+<?php
+        /*Sobrescrevendo a Variável*/
+
+            $nome = 'Weberty Silveira';
+            echo 'Meu nome é ', $nome, '<br>';
+
+            $nome = 'Weberty';
+            echo 'Meu nome é ', $nome, '<br><br>';
+
+
+        /*Tipos de variáveis*/
+
+            /*Strings*/
+            $cliente = 'Rafael';
+
+            /*Valores Inteiros*/
+            $idade = 20;
+
+            echo 'Cliente: ', $cliente, ', Idade: ', $idade;
+?>
